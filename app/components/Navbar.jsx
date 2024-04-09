2929c1dff12d0682a44b8bb4b3e76255ccaa6ca9
alexquar <alexanderquarrie@gmail.com>
2024-04-09 16:13:32 -0400

    router setup

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+export default function Navbar() {
+  return (
+    <nav>
+        <h1 className="mr-auto">U Exchange</h1>
+        <div>
+            <p>Home</p>
+            <p>About</p>
+            <p>Contact</p>
+        </div>
+    </nav>
+  )
+}
