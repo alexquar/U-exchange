421388c5b1509db9cc4790fcab29b4ed19f053cb
alexquar <alexanderquarrie@gmail.com>
2024-04-10 16:34:18 -0400

    about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,8 +2,11 @@
 
 export default function About() {
   return (
-    <div>
-      About
-    </div>
+    <main>
+  <h1 className='text-center pt-5 mt-5 text-2xl'>About the Site</h1>
+      <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  
+      <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  <p className="py-5 text-basic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique minus possimus itaque, accusamus optio, adipisci sequi harum pariatur consequatur quidem quam accusantium molestiae. Sit distinctio dolore doloribus. Debitis, quaerat? </p>  
+ 
+  </main>
   )
 }
