aa047ab1042ad6ef3c2547c38febc825cccebe78
alexquar <alexanderquarrie@gmail.com>
2024-04-11 09:13:48 -0400

    fix footer

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -5,10 +5,9 @@ export default async function MainLayout({ children }) {
   return (
     <>
 <Navbar />
-<div className="flex min-h-full flex-col justify-between">
       {children}
 <Footer />
-</div>
+
     </>
   )
 }
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,9 +2,9 @@
 
 export default function Footer() {
   return (
-<footer class="bg-white rounded-lg shadow my-4 dark:bg-gray-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 U Exchange™. All Rights Reserved.
+<footer class="bg-white fixed w-full bottom-0 left-0 rounded-lg shadow mt-4 p-3">
+    <div class="w-full p-4 flex">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 mr-auto">© 2024 U Exchange™. All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
