a3a974401d98b2298c77da322ea8c6bdcbd7bb91
alexquar <alexanderquarrie@gmail.com>
2024-04-11 09:39:09 -0400

    done

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,22 +2,22 @@
 
 export default function Footer() {
   return (
-<footer class="bg-white fixed w-full bottom-0 left-0 rounded-lg shadow mt-4 p-3">
-    <div class="w-full p-4 flex">
-      <span class="text-md text-primary sm:text-center dark:text-gray-400 mr-auto">© 2024 U Exchange™. All Rights Reserved.
+<footer className="bg-white fixed w-full bottom-0 left-0 rounded-lg shadow mt-4 p-3">
+    <div className="w-full p-4 flex">
+      <span className="text-md text-primary sm:text-center dark:text-gray-400 mr-auto">© 2024 U Exchange™. All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="https://currencyapi.com/" class="hover:underline me-4 md:me-6">API</a>
+            <a href="https://currencyapi.com/" className="hover:underline me-4 md:me-6">API</a>
         </li>
         <li>
-            <a href="/" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+            <a href="/" className="hover:underline me-4 md:me-6">Privacy Policy</a>
         </li>
         <li>
-            <a href="/" class="hover:underline me-4 md:me-6">Licensing</a>
+            <a href="/" className="hover:underline me-4 md:me-6">Licensing</a>
         </li>
         <li>
-            <a href="https://github.com/alexquar/U-exchange" class="hover:underline">Source</a>
+            <a href="https://github.com/alexquar/U-exchange" className="hover:underline">Source</a>
         </li>
     </ul>
     </div>
